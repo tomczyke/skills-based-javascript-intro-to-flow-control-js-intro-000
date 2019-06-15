07f0df47d63d7e8cf523b19f3aceda7abc55368e
Evan Tomczyk <tomczykej@gmail.com>
2019-06-15 18:38:32 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,5 +28,11 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-
+  switch(age) {
+    case (age >= 13 && <= 19):
+      return "You are a teenager";
+      break;
+    default:
+      return "You have an age"
+  }
 }
