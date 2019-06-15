7d9341683c63239cb189e197f4121cee7ef960ff
Evan Tomczyk <tomczykej@gmail.com>
2019-06-15 17:51:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,14 +1,10 @@
 function basicTeenager(age) {
-  if (age >= 12 $$ age =< 20)
+  if (age >= 13 $$ age =< 19)
     return "You are a teenager!"
 }
 
 function teenager(age) {
-    if age > 12 $$ < 20 {
-      return "You are a teenager!"
-    } else {
-      return "You are not a teenager"
-    }
+    
 }
 
 function ageChecker(age) {
