4b8335a912cfa10cb8c31ae585496567d75f7389
Evan Tomczyk <tomczykej@gmail.com>
2019-06-15 19:06:22 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -29,10 +29,25 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age) {
-    case age >= 13:
+    case age = 13:
       return "You are a teenager";
       break;
-    case age <= 19:
+    case age = 14:
+      return "You are a teenager";
+      break;
+    case age = 15:
+      return "You are a teenager";
+      break;
+    case age = 16:
+      return "You are a teenager";
+      break;
+    case age = 17:
+      return "You are a teenager";
+      break;
+    case age = 18:
+      return "You are a teenager";
+      break;
+    case age = 19:
       return "You are a teenager";
       break;
     default:
