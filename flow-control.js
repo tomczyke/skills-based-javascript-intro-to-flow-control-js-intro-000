c2e95425050f1221e0d3183ae5b8930aa1f5cd4e
Evan Tomczyk <tomczykej@gmail.com>
2019-06-15 18:26:37 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,7 +15,7 @@ function teenager(age) {
 function ageChecker(age) {
   if (age >= 13 && age <= 19) {
     return "You are a teenager!";
-  } else if (age < 12) {
+  } else if (age < 13) {
       return "You are a kid"
   } else {
       return "You are a grownup"
